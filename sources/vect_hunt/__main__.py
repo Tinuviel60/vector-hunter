dd08ddb77d34dd10c506761f57e4c23b918e6275
Tinuviel60 <meyfroidt.guillaume@gmail.com>
2026-01-06 22:30:39 +0100

    Boucle principale avec pygame dans __main__

--- sources/vect_hunt/__main__.py
+++ sources/vect_hunt/__main__.py
@@ -1,5 +1,9 @@
 import logging
 
+import pygame
+
+from vect_hunt.game import Game
+from vect_hunt.rendering import Render
 from .logging_config import setup_logging
 
 logger = logging.getLogger(__name__)
@@ -14,8 +18,21 @@ if PROFILING:
     REPORT_NB = 40
 
 """
+Configuration globale de l'application et mise en place de 
+la boucle principale du jeu.
 
+Paramètres graphiques et de simulation:
+- WINDOW_WIDTH: Largeur de la fenêtre en pixels
+- WINDOW_HEIGHT: Hauteur de la fenêtre en pixels
+
+- FPS: Fréquence de rendu graphique en frames par seconde
+- SIM_DT: Pas de temps de la simulation en secondes
 """
+
+WINDOW_WIDTH = 1200
+WINDOW_HEIGHT = 800
+FPS = 60  # Fréquence de rendu graphique (frames par seconde)
+SIM_DT = 1 / FPS  # Pas de temps de la simulation (secondes)
 
 setup_logging(
     log_level=logging.WARNING,  # DEBUG, INFO, WARNING
@@ -24,13 +41,42 @@ setup_logging(
 
 def main():
     """
-   
+    Fonction principale de l'application. 
+    Initialise Pygame, crée la fenêtre,
+    et lance la boucle principale du jeu.
     """
 
+    pygame.init()
+    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pygame.display.set_caption("Vector Hunter")
+    clock = pygame.time.Clock()
+
+
+    game = Game(screen)
+
+    running = True
+    accumulator = 0.0  # Accumulateur de temps réel
 
     while running:
-       # Perform main loop operations here
+        # Temps réel écoulé depuis la dernière frame (en secondes)
+        frame_time = clock.tick(FPS) / 1000.0
 
+        # Accumuler le temps réel
+        accumulator += frame_time
+
+        if accumulator > SIM_DT * 4:
+            logger.warning("Temps d'execution trop long, limitation de l'accumulateur | accumulator=%f | frame_time=%f", accumulator, frame_time)
+            accumulator = SIM_DT * 4  # Limiter l'accumulateur pour éviter les spirales de la mort
+
+        while accumulator >= SIM_DT:
+            # Mettre à jour la simulation avec un pas de temps fixe
+            game.update(SIM_DT)
+            accumulator -= SIM_DT
+
+        # Rendu graphique
+        game.render()
+        
+        # Utiliser pour stopper la boucle lors du profiling
         if PROFILING:
             global TICK_LIMIT
             TICK_LIMIT -= 1
@@ -39,9 +85,13 @@ def main():
 
 
 if __name__ == "__main__":
-    profiler = cProfile.Profile()
-    profiler.enable()
-    main()
-    profiler.disable()
-    stats = pstats.Stats(profiler).sort_stats("cumtime")
-    stats.print_stats(REPORT_NB)
+    # Lancer l'application principale avec ou sans profiling
+    if not PROFILING:
+        main()
+    else:
+        profiler = cProfile.Profile()
+        profiler.enable()
+        main()
+        profiler.disable()
+        stats = pstats.Stats(profiler).sort_stats("cumtime")
+        stats.print_stats(REPORT_NB)
